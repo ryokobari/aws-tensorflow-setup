20cf069344b8db6bb2910bb7136f3c0aa2fde566
ryokobari <ryokobari@gmail.com>
2016-09-03 21:54:40 +0700

    fix error on aws ec2

--- setup-aws-tensorflow.bash
+++ setup-aws-tensorflow.bash
@@ -37,14 +37,20 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
 
 # install anaconda
 wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
-bash Anaconda3-4.0.0-Linux-x86_64.sh -b -p /mnt/bin/anaconda3
+sudo bash Anaconda3-4.0.0-Linux-x86_64.sh -b -p /mnt/bin/anaconda3
 rm Anaconda3-4.0.0-Linux-x86_64.sh
 echo 'export PATH="/mnt/bin/anaconda3/bin:$PATH"' >> ~/.bashrc
 
 # install tensorflow
 export TF_BINARY_URL='https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.9.0rc0-cp35-cp35m-linux_x86_64.whl'
 
-/mnt/bin/anaconda3/bin/pip install $TF_BINARY_URL
+export LC_ALL=en_US.UTF-8
+echo 'export LC_ALL=en_US.UTF-8' >> ~/.bashrc
+
+export LANG=en_US.UTF-8
+echo 'export LANG=en_US.UTF-8' >> ~/.bashrc
+
+sudo /mnt/bin/anaconda3/bin/pip install $TF_BINARY_URL
 
 # install monitoring programs
 sudo wget https://git.io/gpustat.py -O /usr/local/bin/gpustat
